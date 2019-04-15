18520f270d1d3a1a7e022347d5badac92c6fa16b
Pedro <pedromatu29@gmail.com>
2019-04-15 18:32:28 -0400

    arreglo_notas

---
+++ arreglo_notas.rb
@@ -0,0 +1,18 @@
+notas = [5, 7, 1, 3, 5, 8, 9, 'N.A', 'N.A', 3]
+
+def promedio (notas)
+resultado  = []
+notas.each do |nota|
+
+    if nota == 'N.A'
+        resultado.push 2
+    else 
+        resultado.push nota
+        
+    end
+
+    end
+    print resultado
+end
+
+promedio(notas)

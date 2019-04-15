a4bcf9f7a8135fd91ddac43b7533bc528f16e5a4
Pedro <pedromatu29@gmail.com>
2019-04-15 19:08:04 -0400

    desafio1_online

--- desafio1_online.rb
+++ desafio1_online.rb
@@ -1,8 +1,21 @@
-a = [1, 9 ,2, 10, 3, 7, 4, 6]
+a = [1, 9, 2, 10, 3, 7, 4, 6]
 
-b = a.map do |e|
-    e*2
-end
- 
+b = a.map {|x| x.to_f}
 print b
 puts "\n"
+
+c = a.select {|x| x>=5}
+print c
+puts "\n"
+
+d = a.inject(0){|sum,x| sum + x}
+print d
+puts "\n"
+
+e = a.count {|x| x < 5}
+print e
+puts "\n"
+
+
+
+

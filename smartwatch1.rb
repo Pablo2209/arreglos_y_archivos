bfb3e427f4f56ed154cb83c1aec0c975183faad6
Pedro <pedromatu29@gmail.com>
2019-04-15 21:44:58 -0400

    smartwatch_filtro_procesos

--- smartwatch1.rb
+++ smartwatch1.rb
@@ -1,3 +1,9 @@
 pasos = ['100', '21', '231as', '2031', '1052000', '213b', 'b123'].to_i
 
-def clear_steps
+def clear_steps (pasos)
+    steps = []
+    if pasos.to_i < 200 && pasos.to_i > 100000
+        pasos.push
+
+
+

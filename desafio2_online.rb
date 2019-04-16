752af1510ebd39738bc28df509d536f501561a09
Pedro <pedromatu29@gmail.com>
2019-04-16 12:36:51 -0400

    corrección

--- desafio2_online.rb
+++ desafio2_online.rb
@@ -1,4 +1,22 @@
-nombres = ['Violeta', 'Andino', 'Clemente',
-'Javiera', 'Paula', 'Pía', 'Ray']
+nombres = ['Violeta', 'Andino', 'Clemente', 'Javiera', 'Paula', 'Pia', 'Ray', 'Camilo']
 
-b = a.map {}
+a = nombres.select { |x| x.length > 5 } 
+print a 
+puts "\n"
+
+b = nombres.map { |x| x.downcase }
+print b 
+puts "\n"
+
+c = nombres.select { |x| x if x[0] == 'P'}
+print c 
+puts "\n"
+
+d = nombres.count { |x| x[0] == 'A' || x[0] == 'B' || x[0] == 'C'}
+print d
+puts "\n"
+
+e = nombres.map {|x| x.length}
+print e
+puts "\n"
+
--- smartwatch1.rb
+++ smartwatch1.rb
@@ -2,8 +2,6 @@ pasos = ['100', '21', '231as', '2031', '1052000', '213b', 'b123'].to_i
 
 def clear_steps (pasos)
     steps = []
-    if pasos.to_i < 200 && pasos.to_i > 100000
+    
+    if pasos.to_i > 200 && pasos.to_i < 100000
         pasos.push
-
-
-
